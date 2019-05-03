b40ea6d67d1c9dad9d9da1aa6c91cec68992e73c
SekachVitaliy <vitaliy.sekach@gmail.com>
2019-05-03 14:41:46 +0300

    hw with switch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,22 +1,18 @@
 package com.company;
 import java.util.Scanner;
-import java.lang.String;
+
 
 public class Main {
 
     public static void main(String[] args){
         Scanner num = new Scanner(System.in);
-        int first,second=50;
-        boolean isTrue = true;
         System.out.print("Enter the number:");
+        int first;
         first = num.nextInt();
         switch(first){
-            case 51:System.out.print("Number is equel 51"); break;
-            case 91:System.out.print("Number is equel 51"); break;
-            case 61:System.out.print("Number is equel 51"); break;
-            case 71:System.out.print("Number is equel 51"); break;
-        default:
-           System.out.print("Enother number");
+            case 3: System.out.print("3"); break;
+            case 7: System.out.print("7"); break;
+            default: System.out.print("Something else");
 
 
 
