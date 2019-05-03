1fa03f027212d39c393f85f411dea2331e566875
SekachVitaliy <vitaliy.sekach@gmail.com>
2019-05-03 13:42:15 +0300

    Interactive program a+b=c

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,15 +5,13 @@ import java.lang.String;
 public class Main {
 
     public static void main(String[] args){
-           Scanner num = new Scanner(System.in);
-           int first,second,result;
-           System.out.print("First number:");
-           first = num.nextInt();
-           System.out.print("Second number:");
-           second = num.nextInt();
-           result=first + second;
-           result --;
-           System.out.println(first+"+"+second+"="+result);
-
+        Scanner num = new Scanner(System.in);
+        int first,second,result;
+        System.out.print("First number:");
+        first = num.nextInt();
+        System.out.print("Second number:");
+        second = num.nextInt();
+        result=first + second;
+        System.out.println(first+"+"+second+"="+result);
     }
 }
