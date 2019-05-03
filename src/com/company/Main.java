c0d746ad5f213485ce3a741b0f3f734903c97612
SekachVitaliy <vitaliy.sekach@gmail.com>
2019-05-03 15:29:34 +0300

    HW:Create array and find the biggest item

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
     public static void main(String[] args){
        int n;
+       int max;
        int [] a;
         Scanner in = new Scanner(System.in);
         System.out.print("Enter numbers of array:");
@@ -13,6 +14,11 @@ public class Main {
         for(int i = 0;i < n; i++){
             System.out.print("Enter a["+ i +"] = ");
             a[i] = in.nextInt();
-        }
+        }max=a[0];
+        for(int i=1;i<n;i++){
+            if(max<a[i])
+                max=a[i];
+         }
+        System.out.println(max);
     }
 }
