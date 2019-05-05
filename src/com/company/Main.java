05178d1b749e3c203065cb46bb698429121dc3b6
SekachVitaliy <vitaliy.sekach@gmail.com>
2019-05-05 18:35:07 +0300

    Work with constructors

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
-        Person Sasha = new Person();
-        Person Vlad  = new Person();
+        Person Sasha = new Person(145,"Sasha");
+        Person Vlad  = new Person(175,"Vlad");
         Vlad.say("Sasha");
         Sasha.say("Vald");
         Vlad.height =145 ;
-        System.out.print("Vlad "+Vlad.height+"sm and Shasha "+ Sasha.height+"sm");
+        System.out.print(Sasha.name+"'s height "+ Sasha.height+"sm");
 
     }
 
--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -3,12 +3,22 @@ package com.company;
 
  public class Person {
    public int height = 180;
+   public String name;
 
    public void say(String name){
        System.out.println("Hello "+ name +":)");
    }
 
+   public Person (int h,String j){
+       name = j;
+       height = h;
+   }
+
+     public Person (String j) {
+         name = j;
+     }
 
 
 
-}
+
+     }
