0182fe7a6af04a1e112bc1fa51390da9c6854d79
SekachVitaliy <vitaliy.sekach@gmail.com>
2019-05-05 18:21:50 +0300

    create a class with 1metod and 1variable

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,22 +5,14 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
-        write_array();
+        Person Sasha = new Person();
+        Person Vlad  = new Person();
+        Vlad.say("Sasha");
+        Sasha.say("Vald");
+        Vlad.height =145 ;
+        System.out.print("Vlad "+Vlad.height+"sm and Shasha "+ Sasha.height+"sm");
 
     }
-    public static int[] write_array() {
-        int n;
-        int array[];
-        System.out.print("Enter bit depth : ");
-        Scanner in = new Scanner(System.in);
-        n = in.nextInt();
-        array = new int[n];
-        for (int i = 0; i < n; i++) {
-            System.out.print("Enter the number ["+ (i+1) +"]-");
-            array[i] = in.nextInt();
-        }
-        return array;
 
 
-    }
 }
